32520b698da14666e8d2d77be2ed983366adcc86
zuyo7852 <leejuyeon7852@gmail.com>
2025-08-13 23:15:23 +0900

    fix: Report enums

--- src/main/java/hackathon/bigone/sunsak/accounts/mypage/enums/ReportTarget.java
+++ src/main/java/hackathon/bigone/sunsak/accounts/mypage/enums/ReportTarget.java
@@ -1,6 +1,11 @@
 package hackathon.bigone.sunsak.accounts.mypage.enums;
 
 public enum ReportTarget {
-    USER, //사용자
-    POST //게시글
+    USER("사용자"),
+    POST("게시글");
+
+    private String displayName;
+    ReportTarget(String displayName) {
+        this.displayName = displayName;
+    }
 }
--- src/main/java/hackathon/bigone/sunsak/accounts/mypage/enums/ReportType.java
+++ src/main/java/hackathon/bigone/sunsak/accounts/mypage/enums/ReportType.java
@@ -1,8 +1,13 @@
 package hackathon.bigone.sunsak.accounts.mypage.enums;
 
 public enum ReportType {
-    INAPPROPRIATE_CONTENT, // 게시판 성격에 부적절한 대화
-    INSULT,                // 욕설/비하
-    PORNOGRAPHIC,          // 음란물/불건전한 콘텐츠
-    IMPERSONATION          // 사칭/사기
+    INAPPROPRIATE_CONTENT("게시판 성격에 부적절한 대화"),
+    INSULT("욕설/비하"),
+    PORNOGRAPHIC("음란물/불건전한 콘텐츠"),
+    IMPERSONATION("사칭/사기");
+
+    private String displayName;
+    ReportType(String displayName) {
+        this.displayName = displayName;
+    }
 }
