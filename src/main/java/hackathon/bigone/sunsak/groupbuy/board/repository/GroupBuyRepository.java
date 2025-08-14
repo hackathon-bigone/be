182260ac4978e2a9c5b26f0b48c4fc8464600629
yijoo <761610@naver.com>
2025-08-15 01:38:13 +0900

    search

--- src/main/java/hackathon/bigone/sunsak/groupbuy/board/repository/GroupBuyRepository.java
+++ src/main/java/hackathon/bigone/sunsak/groupbuy/board/repository/GroupBuyRepository.java
@@ -1,9 +1,13 @@
 package hackathon.bigone.sunsak.groupbuy.board.repository;
 
 import hackathon.bigone.sunsak.groupbuy.board.entity.Groupbuy;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GroupBuyRepository extends JpaRepository<Groupbuy, Long> {
+    List<Groupbuy> findByGroupbuyTitleContaining(String keyword, Pageable pageable);
 }
