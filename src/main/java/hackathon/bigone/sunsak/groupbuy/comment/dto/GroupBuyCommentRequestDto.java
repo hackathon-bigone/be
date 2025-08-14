f58eb0c923f96a99ccafc299782069c7883364e9
yijoo <761610@naver.com>
2025-08-15 02:24:19 +0900

    fix

--- src/main/java/hackathon/bigone/sunsak/groupbuy/comment/dto/GroupBuyCommentRequestDto.java
+++ src/main/java/hackathon/bigone/sunsak/groupbuy/comment/dto/GroupBuyCommentRequestDto.java
@@ -1,6 +1,5 @@
 package hackathon.bigone.sunsak.groupbuy.comment.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
