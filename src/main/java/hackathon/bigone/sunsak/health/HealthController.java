95a8c4cc81f286c5f7df5c3da20321c750df3c76
zuyo7852 <leejuyeon7852@gmail.com>
2025-08-18 03:05:00 +0900

    test healthClass

---
+++ src/main/java/hackathon/bigone/sunsak/health/HealthController.java
@@ -0,0 +1,9 @@
+package hackathon.bigone.sunsak.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/ping") public String ping() { return "pong"; }
+}
